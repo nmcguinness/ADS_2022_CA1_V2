ebf48ffa23fafea7fe86d315d78271c8612094b1
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-11-07 16:34:03 +0000

    WIP - interleaveArrays

--- ADS_2022_CA1_V2/ADS_2022_CA1_V2.cpp
+++ ADS_2022_CA1_V2/ADS_2022_CA1_V2.cpp
@@ -1,9 +1,36 @@
 // ADS_2022_CA1_V2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#define SHOW_DEBUG_MSGS
 #include <iostream>
+#include "Functions.h"
 
 int main()
 {
+	int z = 100;
+
+#ifdef SHOW_DEBUG_MSGS
+	std::cout << z << std::endl;
+#endif
+
+#ifdef SHOW_DEBUG_MSGS
+	std::cout << z << std::endl;
+#endif
+
+#ifdef SHOW_DEBUG_MSGS
+	std::cout << z << std::endl;
+#endif
+
+#ifdef SHOW_DEBUG_MSGS
+	std::cout << z << std::endl;
+#endif
+
+#pragma region Constructors
 	std::cout << "Hello World!\n";
+	//...
+	//...
+#pragma endregion
+
+#pragma region Getters/Setters
+	int x = 10;
+#pragma endregion
 }
---
+++ ADS_2022_CA1_V2/Functions.h
@@ -0,0 +1,23 @@
+#pragma once
+//pre-compiler directives
+
+//1,2,3,4
+//5,6,7,8
+void interleaveArrays(int* pA1, int
+	lengthA1, int* pA2, int
+	lengthA2, int* pArrOut) {
+	int j = 0;
+	for (int i = 0; i < lengthA1 + lengthA2; i++)
+	{
+		if (i % 2 == 0)
+		{
+			pArrOut[i] = pA1[j];
+			j++;
+		}
+		else
+		{
+			pArrOut[i] = pA2[j];
+			j++;
+		}
+	}
+}
